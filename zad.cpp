2f96146e9afc5df125c3edccfdd3bad13cbb66be
Areklukas <56909985+Areklukas@users.noreply.github.com>
2021-04-21 09:50:57 +0200

    Update zad.cpp

--- zad.cpp
+++ zad.cpp
@@ -15,19 +15,19 @@ int main()
         	getline(plik,line);
     	}
     plik.close();
-    char* char_arr;
+    char*char_arr;
     string str_obj(line);
-    char_arr = &str_obj[0];
+    char_arr=&str_obj[0];
     cout<<char_arr;
 
-    char *magicTest = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"; 
-    char shaSource[128] = char_arr;
-    strcat(shaSource, magicTest); 
-    printf("String kodowany: [%s]\n", shaSource);
-    char result[32];
-    SHA1(result, shaSource, strlen(shaSource) ); 
-    char base64string[128];
-    Base64encode(base64string, result, strlen(result)); 
-    printf("Po zakodowaniu to [%s] - dlugosc %i bajtow", base64string, strlen(base64string)); 
-    return 0;
+        char *magicTest = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"; //36 znakow, GUID Magic string
+        char shaSource[128] = "dGhlIHNhbXBsZSBub25jZQ=="; //24
+        strcat(shaSource, magicTest); //laczymy te stringi w jeden do zakodowania
+        printf("String kodowany: [%s]\n", shaSource);
+        char result[32];//21
+        SHA1(result, shaSource, strlen(shaSource) ); //Kodowanie SHA1
+        char base64string[128];
+        Base64encode(base64string, result, strlen(result)); //konwersja Base64
+        printf("Po zakodowaniu to [%s] - dlugosc %i bajtow", base64string, strlen(base64string)); //wynikowy string, który wyślemy w odpowiedzi HandShake.
+        return 0;
  }
